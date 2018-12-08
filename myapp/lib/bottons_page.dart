6ef0bfe5f5df8decabd2e90cb75530d7f95de942
Arthur Tavares <arthurabreu00@gmail.com>
2018-12-08 20:18:57 -0200

    Lista

--- myapp/lib/bottons_page.dart
+++ myapp/lib/bottons_page.dart
@@ -36,10 +36,24 @@ class _BottonsPageStart extends State<BottonsPage> {
                 child: buildRaisedButtom(
                     'Raised button, adicionam dimensão a layouts simples e enfatiza funções em espaços ocupados ou ambos'),
               ),
-              const Text('Flat'),
-              const Text('Outline'),
-              const Text('Icon'),
-              const Text('Action')
+              ButtonTheme.fromButtonThemeData(
+                data: buttonTheme,
+                child: buildFlatButtom(
+                    'Flat button, é um botão mais limpo. é recomendado utiliza-lo em barras de ferramentas e nas caixa de dialagos'),
+              ),
+              ButtonTheme.fromButtonThemeData(
+                data: buttonTheme,
+                child: buildOutlineButtom(
+                    'Outline button, são opacos e elevam-se quando pressionados. Eles são frequentemente usados emparelhados com botões em relevo, para indicar uma ação secundaria.'),
+              ),
+              ButtonTheme.fromButtonThemeData(
+                data: buttonTheme,
+                child: buildIconButtom(),
+              ),
+              ButtonTheme.fromButtonThemeData(
+                data: buttonTheme,
+                child: buildActionButtom(),
+              )
             ],
           ),
         ),
@@ -48,6 +62,7 @@ class _BottonsPageStart extends State<BottonsPage> {
   }
 }
 
+// RaisedButtom
 Widget buildRaisedButtom(title) {
   return new Align(
     alignment: const Alignment(0.0, -0.2),
@@ -79,7 +94,7 @@ Widget buildRaisedButtom(title) {
             label: const Text('Raised Button'),
             onPressed: () {},
           ),
-                    new RaisedButton.icon(
+          new RaisedButton.icon(
             icon: const Icon(
               Icons.add,
               size: 18.0,
@@ -92,3 +107,125 @@ Widget buildRaisedButtom(title) {
     ),
   );
 }
+
+// FlatButtom
+Widget buildFlatButtom(title) {
+  return new Align(
+    alignment: const Alignment(0.0, -0.2),
+    child: new Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new Text(
+          title,
+          textAlign: TextAlign.center,
+          style: new TextStyle(fontWeight: FontWeight.bold),
+        ),
+        new ButtonBar(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            new FlatButton(
+              child: const Text('FLAT BUTTON'),
+              onPressed: () {},
+            ),
+            const FlatButton(child: const Text('DESABILITADO'), onPressed: null)
+          ],
+        ),
+        new ButtonBar(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          new FlatButton.icon(
+            icon: const Icon(
+              Icons.add,
+              size: 18.0,
+            ),
+            label: const Text('Flat Button'),
+            onPressed: () {},
+          ),
+          new FlatButton.icon(
+            icon: const Icon(
+              Icons.add,
+              size: 18.0,
+            ),
+            label: const Text('Desabilitado'),
+            onPressed: null,
+          )
+        ])
+      ],
+    ),
+  );
+}
+
+// OutlineButtom
+Widget buildOutlineButtom(title) {
+  return new Align(
+    alignment: const Alignment(0.0, -0.2),
+    child: new Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new Text(
+          title,
+          textAlign: TextAlign.center,
+          style: new TextStyle(fontWeight: FontWeight.bold),
+        ),
+        new ButtonBar(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            new OutlineButton(
+              child: const Text('Outline BUTTON'),
+              onPressed: () {},
+            ),
+            const OutlineButton(
+                child: const Text('DESABILITADO'), onPressed: null)
+          ],
+        ),
+        new ButtonBar(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          new OutlineButton.icon(
+            icon: const Icon(
+              Icons.add,
+              size: 18.0,
+            ),
+            label: const Text('Outline Button'),
+            onPressed: () {},
+          ),
+          new OutlineButton.icon(
+            icon: const Icon(
+              Icons.add,
+              size: 18.0,
+            ),
+            label: const Text('Desabilitado'),
+            onPressed: null,
+          )
+        ])
+      ],
+    ),
+  );
+}
+
+Widget buildIconButtom() {
+  return Align(
+    alignment: const Alignment(0.0, -0.2),
+    child: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.thumb_up),
+          onPressed: (){},
+        ),
+        IconButton(
+          icon: const Icon(Icons.thumb_down),
+          onPressed: null,
+        )
+      ],
+    ),
+  );
+} 
+
+Widget buildActionButtom() {
+  return Align(
+    alignment: const Alignment(0.0, -0.2),
+    child: FloatingActionButton(
+      child: const Icon(Icons.add),
+      onPressed: (){},
+    )
+  );
+}
+
+
